29792dda1611060566125fecf38e1685d308b7de
krtn <cranriar@hotmail.com>
2025-06-18 21:10:22 -0500

    install vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+import { createApp } from 'vue';
+import ExampleComponent from './components/ExampleComponent.vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
+console.log('Vue está cargando...');
+
+const app = createApp({});
+app.component('example-component', ExampleComponent);
+app.mount('#app');
